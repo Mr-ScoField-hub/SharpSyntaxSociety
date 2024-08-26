22f352a2568bf3bc27df10b2ef8ee00e998c2162
u-Wandile <uslindile.dou@gmail.com>
2024-08-26 09:59:58 +0200

    new buttin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.testButton.Font = new System.Drawing.Font("Helvetica Rounded", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testButton.ForeColor = System.Drawing.Color.Red;
+            this.testButton.Location = new System.Drawing.Point(330, 155);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(185, 112);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "button1";
+            this.testButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button testButton;
     }
 }
 
